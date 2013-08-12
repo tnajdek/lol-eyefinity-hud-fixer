734307288f72154c37b886270bb2208a35904872
Tom Najdek <tom@doppnet.com>
2013-08-12 01:24:44 +0100

    WIP

--- hudfixer.py
+++ hudfixer.py
@@ -1,14 +1,127 @@
 #!/usr/bin/env python
+from __future__ import division
+import re
 
-class Vec2():
+# Single screen resolution
+TARGET_SCREEN_RESOLUTION = 1920
+
+# Max value LOL will allow vertically from the anchor point
+# e.g. if anchor point is 1,1 and Rect start is 0,0 then
+# rect will be placed MAGIC_VALUE pixels from the right-most
+# value
+MAGIC_VALUE = 1440
+
+RATIO = TARGET_SCREEN_RESOLUTION / MAGIC_VALUE
+
+class Vec2(object):
 	def __init__(self, x, y):
 		self.x = x or 0.0
 		self.y = y or 0.0
 
-class Rect():
+class Rect(object):
 	def __init__(self, start, end):
 		self.start = start
 		self.end = end
 
+class LolRect(Rect):
+	def __init__(self, start, end, res_w, res_h):
+		super(LolRect, self).__init__(start, end)
+		self.res_w = res_w
+		self.res_h = res_h
+
+def get_abs_scaled_rect(rect, res_w, res_h):
+	return Rect(
+		Vec2(
+			rect.start.x / res_w,
+			rect.start.y / res_h
+		),
+		Vec2(
+			rect.end.x / res_w,
+			rect.end.y / res_h
+		)
+	)
+
 def get_lol_scaled_rect(rect, res_w, res_h):
-	
+	return LolRect(
+		Vec2(
+			rect.start.x * res_w,
+			rect.start.y * res_h
+		),
+		Vec2(
+			rect.end.x * res_w,
+			rect.end.y * res_h
+		),
+		res_w, res_h
+	)
+
+# rect must be abs scaled!
+def reanchor_centrally(rect, anchor_src):
+	conversion_ratio = 1 + (RATIO - 1)/2
+
+	new_rect = Rect(
+		Vec2(
+			rect.start.x,
+			rect.start.y
+		),
+		Vec2(
+			rect.end.x,
+			rect.end.y
+		)
+	)
+	if(anchor_src.x == 1):
+		new_rect.start.x = rect.start.x * conversion_ratio
+		new_rect.end.x = rect.end.x * conversion_ratio
+	elif(anchor_src.x != 0.5):
+		raise NotImplementedError()
+
+	return new_rect
+
+def parse_fragment(fragment):
+	parsed = dict()
+	lines = fragment.split("\n")
+	for line in lines:
+		line = line.strip()
+		if(line):
+			key, value = re.match("(\w+):\s*(.*)", line).groups()
+			if(key == 'Rect'):
+				rect_spec = re.match("\s*([\d\.]+),([\d\.]+)[\s\-]+([\d\.]+),([\d\.]+)[\s\/]+(\d+)x(\d+)", value).groups()
+				value = LolRect(
+					Vec2(*[float(x) for x in rect_spec[0:2]]),
+					Vec2(*[float(x) for x in rect_spec[2:4]]),
+					*[int(x) for x in rect_spec[4:6]]
+				)
+			elif(key == 'Anchor'):
+				anchor_spec = re.match("\s*([\d\.]+)[\s,]*([\d\.]+)", value).groups()
+				value = Vec2(*[float(x) for x in anchor_spec])
+			parsed[key] = value
+	return parsed
+
+def parse_fragments(fragments):
+	fragments_collection = list()
+	split_fragments = re.split('/{5,}', fragments)
+	for split_fragment in split_fragments:
+		parsed_fragment = parse_fragment(split_fragment)
+		fragments_collection.append(parsed_fragment)
+	return fragments_collection
+
+def compile_fragment(fragment_dict):
+	fragment = ''
+	for key,value in fragment_dict.iteritems():
+		if(key == "Rect"):
+			value = "%.2f,%.2f - %.2f,%.2f / %ix%i" % (
+						value.start.x, value.start.y,
+						value.end.x, value.end.y, 
+						value.res_w, value.res_h
+			)
+		if(key == "Anchor"):
+			value = "%.2f,%.2f" % (value.x, value.y)
+		fragment = fragment + "%s: %s\n" % (key, value)
+	return fragment
+
+def compile_fragments(fragments):
+	separator = "//////////////////////////////////////////\n"
+	fragments = [compile_fragment(f) for f in fragments]
+	return separator.join(fragments)
+
+def process(fragment):
+	pass
--- test.py
+++ test.py
@@ -1,20 +1,97 @@
 #!/usr/bin/env python
-from hudfixer import Vec2, Rect, anchor_centrally
+import unittest
+from hudfixer import (
+    Vec2, Rect, LolRect, reanchor_centrally, get_abs_scaled_rect,
+    get_lol_scaled_rect, parse_fragment, parse_fragments,
+    compile_fragment, compile_fragments
+)
 
-class TestReanchorCalculation(unittest.TestCase):
+
+class TestReanchoring(unittest.TestCase):
     def setUp(self):
-        pass
+        self.fragment = "Type: Icon\n\
+            Name: MinimapFrame\n\
+            Group: Minimap\n\
+            Enabled: 1\n\
+            KeepMaxScale: 1\n\
+            AlwaysRespond: 0\n\
+            Draggable: 0\n\
+            Layer: 2\n\
+            Anchor: 1,1\n\
+            Rect: 829.44,544.0 - 1024.0,768.64 / 1024x768\n\
+            Alpha: 1\n\
+            Texture: HUDAtlas\n\
+            UV: 87,7 - 391,358 / 1024x1024\n"
+
+        self.fragments = "Type: Icon\n\
+            Name: MinimapTooltip\n\
+            Group: Minimap\n\
+            Enabled: 0\n\
+            KeepMaxScale: 1\n\
+            AlwaysRespond: 0\n\
+            Draggable: 0\n\
+            Layer: 1\n\
+            Anchor: 1,1\n\
+            Rect: 1013.12,522.88 - 1013.76,523.52 / 1024x768\n\
+            Alpha: 1\n\
+            Texture: None\n\
+            UV: 0,0 - 1,1\n\
+            //////////////////////////////////////////\n\
+            Type: Icon\n\
+            Name: MinimapPillar\n\
+            Group: Minimap\n\
+            Enabled: 1\n\
+            KeepMaxScale: 1\n\
+            AlwaysRespond: 0\n\
+            Draggable: 0\n\
+            Layer: 1\n\
+            Anchor: 1,1\n\
+            Rect: 778.24,542.72 - 829.44,769.28 / 1024x768\n\
+            Alpha: 1\n\
+            Texture: HUDAtlas\n\
+            UV: 5,5 - 85,358 / 1024x1024\n"
         
     def test_anchor_centrally(self):
-        old_rect = Rect(Vec2(829.44, 544.0), Vec2(1024.0, 768.64))
-        reanchored_rect = anchor_centrally(old_rect)
+        old_rect = Rect(Vec2(829.44, 576.0), Vec2(1020.16, 763.52))
+        old_rect = get_abs_scaled_rect(old_rect, 1024, 768)
+        reanchored = reanchor_centrally(old_rect, Vec2(1, 1))
+        repositioned = get_lol_scaled_rect(reanchored, 1440, 1080)
 
-        self.assertEqual(reanchored.start.x, 1410)
-        self.assertEqual(reanchored.start.y, 810)
-        self.assertEqual(reanchored.end.x, 1674)
-        self.assertEqual(reanchored.end.y, 1074)
-        self.assertEqual(reanchored.scale.x, 1440)
-        self.assertEqual(reanchored.scale.y, 1080)
+        self.assertAlmostEqual(repositioned.start.x, 1361, places=0)
+        self.assertAlmostEqual(repositioned.start.y, 810, places=0)
+        self.assertAlmostEqual(repositioned.end.x, 1674, places=0)
+        self.assertAlmostEqual(repositioned.end.y, 1074, places=0)
 
+    def test_fragment_parsing(self):
+        parsed = parse_fragment(self.fragment)
+        self.assertIsInstance(parsed['Rect'], LolRect)
+        self.assertIsInstance(parsed['Anchor'], Vec2)
+        self.assertEqual(parsed['Rect'].start.x, 829.44)
+        self.assertEqual(parsed['Rect'].start.y, 544.0)
+        self.assertEqual(parsed['Rect'].end.x, 1024.0)
+        self.assertEqual(parsed['Rect'].end.y, 768.64)
+        self.assertEqual(parsed['Rect'].res_w, 1024)
+        self.assertEqual(parsed['Rect'].res_h, 768)
+        self.assertEqual(parsed['Texture'], 'HUDAtlas')
+
+    def test_fragments_parsing(self):
+        parsed = parse_fragments(self.fragments)
+        self.assertEqual(len(parsed), 2)
+        self.assertIsInstance(parsed[0]['Rect'], LolRect)
+        self.assertEqual(parsed[0]['Rect'].start.x, 1013.12)
+        self.assertEqual(parsed[1]['Rect'].start.x, 778.24)
+
+    def test_fragment_writing(self):
+        processed = compile_fragment(parse_fragment(self.fragment))
+        self.assertEqual(len(parse_fragment(processed)), len(parse_fragment(self.fragment)))
+
+    def test_fragments_writing(self):
+        processed = compile_fragments(parse_fragments(self.fragments))
+        self.assertEqual(len(parse_fragment(processed)), len(parse_fragment(self.fragment)))        
+
+    def test_fragment_processing(self):
+        pass
+
+        
 if __name__ == '__main__':
     unittest.main()
